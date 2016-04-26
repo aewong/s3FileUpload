4936ed6ab76d7205e2d5b7cbb746a0825f3bb0e4
Audrey Wong <aewong@asu.edu>
2016-04-25 21:01:41 -0700

    Added login stuff

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ AWS.config.loadFromPath("./credentials.json");
 var s3 = new AWS.S3();
 
 var express = require("express");
+var session = require("express-session");
+var userLib = require("./user.js");
 var app = express();
 var bodyParser = require("body-parser");
 var errorHandler = require("errorhandler");
 var methodOverride = require("method-override");
 var hostname = process.env.HOSTNAME || "localhost";
 var port = 8080;
+var db = require("./node_modules/mongoskin").db("mongodb://user:password@127.0.0.1:27017/s3FileUpload");
+
+app.use(session({secret: "This is a secret"}));
 
 app.get("/", function(req, res) {
     res.redirect("/index.html");
@@ -37,6 +42,26 @@ app.post("/uploadFile", function(req, res) {
         });
     });
 });
+
+app.get("/createUser", function(req, res) {
+        userLib.add(req, res, db);
+        });
+
+app.get("/editUser", function(req, res) {
+        userLib.edit(req, res, db);
+        });
+
+app.get("/changePassword", function(req, res) {
+        userLib.changePassword(req, res, db);
+        });
+
+app.get("/login", function(req, res) {
+        userLib.login(req, res, db);
+        });
+
+app.get("/getUser", function(req, res) {
+        userLib.get(req, res, db);
+        });
 
 app.use(methodOverride());
 app.use(bodyParser());
